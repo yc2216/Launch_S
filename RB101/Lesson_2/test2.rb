46a6a7fb24c29cb24c499f4e4a35ec68458bcc64
Maya angelou <MayaAngelou@poets.com>
2018-12-27 20:52:14 -0500

    12/27 01

---
+++ RB101/Lesson_2/test2.rb
@@ -0,0 +1,7 @@
+#this is test #2
+#trying to push this file to github
+#last time it fails, then I have to use
+#git pull -rebase origin master
+#before I use
+#git push origin master
+#again to get it done.
